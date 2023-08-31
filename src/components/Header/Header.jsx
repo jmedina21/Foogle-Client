a8f279bf559db816d9e50979770567ff486bd550
Pablo Medina <jmedinamulet@gmail.com>
2023-08-31 10:07:24 -0400

    update header to new styles, mobile only

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,23 @@
 import './Header.scss'
-// import search from '../../assets/icons/search.svg'
 import search from '../../assets/icons/MagnifyingGlass.svg'
-import logo from '../../assets/logos/foogle-logo.svg'
+import login from '../../assets/icons/login.svg'
+import logo from '../../assets/logos/foogle_logo.png'
+import { Link } from 'react-router-dom'
 
 export function Header(){
 
     return (
-        <header className='search'>
-            <div className='search__container'>
-                <div className='search__logo-input-cont'>
-                    <input type="text" className='search__search-bar' placeholder="Search all second hand market from one place"/>
-                    <img className='search__logo' src={logo} alt="logo" />
+        <header className='header'>
+            <Link to={'/'}> <img className='header__logo' src={logo} alt="logo" /> </Link>
+            <Link to={'/signin'}>
+                <div className='header__signin'>
+                    <p className='header__signin-text'>SIGN IN</p>
+                    <img src={login} alt="login" className='header__signin-icon' />
                 </div>
-                <img className='search__search-icon' src={search} alt="search" />
+            </Link>
+            <div className='header__input-cont'>
+                <input type="text" className='header__search-bar' placeholder="Search all second hand market from one place"/>
+                <img className='header__search-icon' src={search} alt="search" />
             </div>
         </header>
     )
