6596b8b8a43fd37f791973ca4abcbe29271d98c2
Pablo Medina <jmedinamulet@gmail.com>
2023-08-31 15:02:52 -0400

    refactor to take in BurgerMenu

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,40 @@
 import './Home.scss'
-import search from '../../assets/icons/search.svg'
+import searchIcon from '../../assets/icons/MagnifyingGlass.svg'
+import logo from '../../assets/logos/foogle_logo.png'
+import { slide as Menu } from 'react-burger-menu'    
+import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import { BurgerMenu } from '../../components/Menu/BurgerMenu'
 
 export function Home(){
 
+    const [search, setSearch] = useState('')
+    const navigate = useNavigate()
+
+    function handleChange(e){
+        setSearch(e.target.value)
+    }
+
+    function searchOnClick(){
+        navigate(`search/${search}`)
+    }
+
+    function searchOnKeyDown(e){
+        if(e.key === 'Enter'){
+            navigate(`search/${search}`)
+        }
+    }
+
     return (
         <main className='home'>
-            <h1 className='home__title'>foogle</h1>
-            <div className='home__search-section'>
-                <input type="text" className='home__search-bar' placeholder="Search all second hand market from one place"/>
-                <img className='home__search-icon' src={search} alt="search" />
+            <BurgerMenu />
+            <div className='home__container'>
+                <img src={logo} alt="logo" className='home__logo' />
+                <div className='home__search-section'>
+                    <input type="text" className='home__search-bar' onChange={handleChange} onKeyDown={searchOnKeyDown} placeholder="Search all second hand market from one place"/>
+                    <p className='home__search-btn' onClick={searchOnClick} >{`>`}</p>
+                    <img className='home__search-icon' src={searchIcon} alt="search" />
+                </div>
             </div>
         </main>
     )
