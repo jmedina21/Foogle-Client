291c8cd48c14417603a984df7b0c0f5b046f34e4
Pablo Medina <jmedinamulet@gmail.com>
2023-09-02 19:29:44 -0400

    signup/login route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home/Home'
 import { Search } from './pages/Search/Search'
+import { Signup } from './pages/Signup/Signup'
 
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="search/:item" element={<Search />} />
                 <Route path="favorites/" element={<h1>Your Favs</h1>} />
-                <Route path="signin/" element={<h1>Signin here</h1>} />
+                <Route path="signin/" element={<Signup />} />
                 <Route path="*" element={<h1>Page not found</h1>} />
             </Routes>
         </BrowserRouter>
