fae7416c5b519f4127850f56d7ee274c8be4ca29
Pablo Medina <jmedinamulet@gmail.com>
2023-09-02 21:51:56 -0400

    add validation and error responsiveness

--- src/pages/Signup/Signup.jsx
+++ src/pages/Signup/Signup.jsx
@@ -2,23 +2,38 @@ import './Signup.scss'
 import { useState } from 'react'
 import logo from '../../assets/logos/Foogle_logo.png'
 import back from '../../assets/icons/back.svg'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
 export function Signup(){
 
+    const navigate = useNavigate()
+
     const [newAccount, setNewAccount] = useState(false)
     const [ loginData, setLoginData ] = useState({
-        user: '',
+        email: '',
         password: ''
     })
 
     const [ signupData, setSignupData ] = useState({
-        username: '',
         email: '',
         password: '',
         confirmPassword: ''
     })
+
+    const [alert, setAlert] = useState(false)
+    const [alertMessage, setAlertMessage] = useState('')
+
+    const [emailAlert, setEmailAlert] = useState(false)
+    const [passwordAlert, setPasswordAlert] = useState(false)
+    
+    function handleNewAccount(){
+        setNewAccount(!newAccount)
+        setAlert(false)
+        setAlertMessage('')
+        setEmailAlert(false)
+        setPasswordAlert(false)
+    }
 
     function handleLoginChange(e){
         const { name, value } = e.target
@@ -36,28 +51,63 @@ export function Signup(){
         })
     }
 
-    function handleNewAccount(){
-        setNewAccount(!newAccount)
-    }
-
     function handleLogin(e){
         e.preventDefault()
         axios
-            .post('http://localhost:2121/login', {
-                username: loginData.user,
-                email: loginData.user,
-                password: loginData.password
-            })
+            .post('http://localhost:2121/login', loginData)
             .then(res => {
-                console.log(res.data)
+                localStorage.setItem('token', res.data)
+                navigate('/')
             })
             .catch(err => {
-                console.log(err)
+                setAlert(true)
+                setAlertMessage(err.response.data)                
             })
     }
 
     function handleSignup(e){
         e.preventDefault()
+        if (!emailValidation()){
+            setAlert(true)
+            setEmailAlert(true)
+            setAlertMessage('Please enter a valid email')
+            return
+        }else setEmailAlert(false)
+        if (!passwordMatch()){
+            setAlert(true)
+            setPasswordAlert(true)
+            setAlertMessage('Passwords do not match')
+            return
+        }else setPasswordAlert(false)
+        axios
+            .post('http://localhost:2121/signup', {
+                email: signupData.email,
+                password: signupData.password
+            })
+            .then(res => {
+                localStorage.setItem('token', res.data)
+                navigate('/')
+            })
+            .catch(err => {
+                setAlert(true)
+                setAlertMessage(err.response.data)
+            })
+    }
+
+    function passwordMatch(){
+        if(signupData.password === signupData.confirmPassword){
+            return true
+        } else {
+            return false
+        }
+    }
+
+    function emailValidation(){
+        if (signupData.email.includes('@') && signupData.email.includes('.')){
+            return true
+        } else {
+            return false
+        }
     }
 
     return (
@@ -71,17 +121,16 @@ export function Signup(){
                 {!newAccount ?
                 <form className='sign__form' onSubmit={handleLogin}>
                     <h2 className='sign__form-title'>Login to your account</h2>
-                    <input className='sign__input' type="text" name='user' value={loginData.user} onChange={handleLoginChange} placeholder="Email or Username" />
+                    <input className='sign__input' type="text" name='email' value={loginData.email} onChange={handleLoginChange} placeholder="johndoe@gmail.com" />
                     <input className='sign__input' type="password" name='password' value={loginData.password} onChange={handleLoginChange} placeholder="Password" />
                     <button className='sign__btn' type="submit">Login</button>
                 </form>
                     :
                 <form className='sign__form' onSubmit={handleSignup}>
                     <h2 className='sign__form-title'>Create a new account</h2>
-                    <input className='sign__input' type="text" name='username' value={signupData.username} onChange={handleSignupChange} placeholder="Username" />
-                    <input className='sign__input' type="text" name='email' value={signupData.email} onChange={handleSignupChange} placeholder="Email" />
-                    <input className='sign__input' type="password" name='password' value={signupData.password} onChange={handleSignupChange} placeholder="Password" />
-                    <input className='sign__input' type="password" name='confirmPassword' value={signupData.confirmPassword} onChange={handleSignupChange} placeholder="Confirm Password" />
+                    <input className={`sign__input ${emailAlert ? 'sign__input-alert' : ''}`} type="text" name='email' value={signupData.email} onChange={handleSignupChange} placeholder="Email" />
+                    <input className={`sign__input ${passwordAlert ? 'sign__input-alert' : ''}`} type="password" name='password' value={signupData.password} onChange={handleSignupChange} placeholder="Password" />
+                    <input className={`sign__input ${passwordAlert ? 'sign__input-alert' : ''}`} type="password" name='confirmPassword' value={signupData.confirmPassword} onChange={handleSignupChange} placeholder="Confirm Password" />
                     <button className='sign__btn' type="submit">Create Account</button>
                 </form>
                 }
@@ -89,6 +138,9 @@ export function Signup(){
                 <p>Don't have an account?<span className='sign__switch-form' onClick={handleNewAccount}> Sign up</span></p>
                 :
                 <p>Already have an account?<span className='sign__switch-form' onClick={handleNewAccount}> Login</span></p>
+                }
+                {alert &&
+                <p className='sign__alert'>{alertMessage}</p>
                 }
         </main>
     )
