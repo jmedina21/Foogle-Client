39b04d9a238ed512aae6e5cd362dd117886e76db
jmedina21 <jmedinamulet@gmail.com>
2024-09-03 11:10:09 -0400

    add health call to spin up server

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,84 +1,123 @@
-import './Home.scss'
-import searchIcon from '../../assets/icons/MagnifyingGlass.svg'
-import logo from '../../assets/logos/Foogle_logo.png'
-import { Link, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import { BurgerMenu } from '../../components/Menu/BurgerMenu'
-import login from '../../assets/icons/login.svg'
-import favorites from '../../assets/icons/favorites.svg'
-import logout from '../../assets/icons/logout.svg'
-import bmc from '../../assets/icons/bmc-logo.svg'
+import "./Home.scss";
+import searchIcon from "../../assets/icons/MagnifyingGlass.svg";
+import logo from "../../assets/logos/Foogle_logo.png";
+import { Link, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { BurgerMenu } from "../../components/Menu/BurgerMenu";
+import login from "../../assets/icons/login.svg";
+import favorites from "../../assets/icons/favorites.svg";
+import logout from "../../assets/icons/logout.svg";
+import bmc from "../../assets/icons/bmc-logo.svg";
+import axios from "axios";
 
-export function Home(){
-
-    const [isLogged, setIsLogged] = useState(false)
-    const [search, setSearch] = useState('')
-    const navigate = useNavigate()
+export function Home() {
+    const [isLogged, setIsLogged] = useState(false);
+    const [search, setSearch] = useState("");
+    const navigate = useNavigate();
 
     useEffect(() => {
-        if(sessionStorage.getItem('token') && sessionStorage.getItem('token') !== 'undefined'){
-            setIsLogged(true)
+        if (
+            sessionStorage.getItem("token") &&
+            sessionStorage.getItem("token") !== "undefined"
+        ) {
+            setIsLogged(true);
         }
-    }, [])
-    
-    function logOut(){
-        sessionStorage.removeItem('token')
-        setIsLogged(false)
+        axios
+            .get(`${import.meta.env.VITE_API_URL}/health`)
+            .then((res) => console.log(res.data));
+    }, []);
+
+    function logOut() {
+        sessionStorage.removeItem("token");
+        setIsLogged(false);
     }
 
-    function handleChange(e: React.ChangeEvent<HTMLInputElement>){
-        setSearch(e.target.value)
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setSearch(e.target.value);
     }
 
-    function searchOnClick(){
-        navigate(`search/${search}`)
+    function searchOnClick() {
+        navigate(`search/${search}`);
     }
 
-    function searchOnKeyDown(e: React.KeyboardEvent<HTMLInputElement>){
-        if(e.key === 'Enter'){
-            navigate(`search/${search}`)
+    function searchOnKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+        if (e.key === "Enter") {
+            navigate(`search/${search}`);
         }
     }
 
     return (
-        <main className='home'>
+        <main className="home">
             <BurgerMenu isLogged={isLogged} logOut={logOut} />
-            <nav className='home__nav'>
-                {!isLogged ?
-                <Link to={'/login'} className="home__item">
-                    <img className='home__item-icon' src={login} alt="signing" />
-                    <p className='home__item-text'>SIGN IN</p>
-                </Link>
-                :
-                <div className='home__item' onClick={logOut} >
-                    <img src={logout} alt="logout" className='home__item-icon' />
-                    <p className='home__item-text' >LOG OUT</p>
-                </div>
-                }
-                {isLogged ?
-                <Link to={'/favorites'} className="home__item">
-                    <img className='home__item-icon' src={favorites} alt="signing" />
-                    <p className='home__item-text'>FAVORITES</p>
-                </Link>
-                :
-                <div className='home__item home__item--disabled'>
-                    <img className='home__item-icon' src={favorites} alt="signing" />
-                    <p className='home__item-text'>FAVORITES</p>
-                </div>
-                }
-                <a href="https://www.buymeacoffee.com/jpmm21" target='_blank' className="header__item">
-                    <img className='home__item-icon' src={bmc} alt="signing" />
-                    <p className='home__item-text'>BUY ME A COFFEE</p>
+            <nav className="home__nav">
+                {!isLogged ? (
+                    <Link to={"/login"} className="home__item">
+                        <img
+                            className="home__item-icon"
+                            src={login}
+                            alt="signing"
+                        />
+                        <p className="home__item-text">SIGN IN</p>
+                    </Link>
+                ) : (
+                    <div className="home__item" onClick={logOut}>
+                        <img
+                            src={logout}
+                            alt="logout"
+                            className="home__item-icon"
+                        />
+                        <p className="home__item-text">LOG OUT</p>
+                    </div>
+                )}
+                {isLogged ? (
+                    <Link to={"/favorites"} className="home__item">
+                        <img
+                            className="home__item-icon"
+                            src={favorites}
+                            alt="signing"
+                        />
+                        <p className="home__item-text">FAVORITES</p>
+                    </Link>
+                ) : (
+                    <div className="home__item home__item--disabled">
+                        <img
+                            className="home__item-icon"
+                            src={favorites}
+                            alt="signing"
+                        />
+                        <p className="home__item-text">FAVORITES</p>
+                    </div>
+                )}
+                <a
+                    href="https://www.buymeacoffee.com/jpmm21"
+                    target="_blank"
+                    className="header__item"
+                >
+                    <img className="home__item-icon" src={bmc} alt="signing" />
+                    <p className="home__item-text">BUY ME A COFFEE</p>
                 </a>
             </nav>
-            <div className='home__container'>
-                <img src={logo} alt="logo" className='home__logo' />
-                <div className='home__search-section'>
-                    <input type="text" className='home__search-bar' onChange={handleChange} onKeyDown={searchOnKeyDown} placeholder="Search all second hand markets from one place"/>
-                    <p className='home__search-btn' onClick={searchOnClick} >{`>`}</p>
-                    <img className='home__search-icon' src={searchIcon} alt="search" />
+            <div className="home__container">
+                <img src={logo} alt="logo" className="home__logo" />
+                <div className="home__search-section">
+                    <input
+                        type="text"
+                        className="home__search-bar"
+                        onChange={handleChange}
+                        onKeyDown={searchOnKeyDown}
+                        placeholder="Search all second hand markets from one place"
+                    />
+                    <p
+                        className="home__search-btn"
+                        onClick={searchOnClick}
+                    >{`>`}</p>
+                    <img
+                        className="home__search-icon"
+                        src={searchIcon}
+                        alt="search"
+                    />
                 </div>
             </div>
         </main>
-    )
+    );
 }
